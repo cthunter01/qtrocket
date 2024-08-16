cb1a8c379b3fed60e480ee2ff67783e4af013645
Travis Hunter <travis@travishunter.io>
2024-08-16 16:18:35 -0600

    Initial commit of propagator test branch

--- main.cpp
+++ main.cpp
@@ -1,27 +1,23 @@
-/// \cond
-// C headers
-// C++ headers
-// 3rd party headers
-/// \endcond
+#include "MainWindow.h"
 
-#include "QtRocket.h"
-#include "utils/Logger.h"
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
+    QApplication a(argc, argv);
 
-   // Instantiate logger
-   utils::Logger* logger = utils::Logger::getInstance();
-   logger->setLogLevel(utils::Logger::PERF_);
-   logger->info("Logger instantiated at PERF level");
-   // instantiate QtRocket
-   logger->debug("Starting QtRocket");
-   QtRocket* qtrocket = QtRocket::getInstance();
-
-   // Run QtRocket. This'll start the GUI thread and block until the user
-   // exits the program
-   logger->debug("QtRocket->run()");
-   int retVal = qtrocket->run(argc, argv);
-   logger->debug("Returning");
-   return retVal;
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "untitled_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
